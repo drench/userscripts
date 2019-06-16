d402c67be523e78b7076c6b5aca94e1cef33a0f6
drench <drench+github@gmail.com>
2019-06-16 17:54:23 -0500

    Hardcode the height

--- rubydoc-version-switcher.user.js
+++ rubydoc-version-switcher.user.js
@@ -102,7 +102,7 @@ var input = document.createElement('input');
 input.setAttribute('list', rubydoc.versions.id);
 input.setAttribute('autocomplete', 'off');
 input.setAttribute('placeholder', 'Ruby version');
-input.style.height = document.defaultView.getComputedStyle(rd_action_search, '').height;
+input.style.height = '1.3em';
 
 input.addEventListener('change', function (event) {
   rubydoc.changeVersion(event.target.value);
