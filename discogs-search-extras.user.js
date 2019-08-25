80ef8d00db07792ed6c0ade2142dda503ef07154
drench <drench+github@gmail.com>
2019-08-25 17:26:26 -0500

    Remove any parenthesized digit suffixes from artist names

--- discogs-search-extras.user.js
+++ discogs-search-extras.user.js
@@ -57,7 +57,7 @@ document.querySelectorAll('meta[property="og:title"]').forEach(function (metaTag
   var m = ogTitle.match(/^(.+) - (.+)$/);
   if (!m) return;
 
-  var artist = m[1];
+  var artist = m[1].replace(/\s+\(\d+\)$/, '');
   var title = m[2];
 
   var profile = document.querySelector('#page_content .body .profile');
