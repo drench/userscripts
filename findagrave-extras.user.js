f3307991e84b8a774dd180c0aa24dfdd39e882db
drench <drench+github@gmail.com>
2020-03-08 11:46:32 -0500

    Make familysearch link buttons

--- findagrave-extras.user.js
+++ findagrave-extras.user.js
@@ -12,7 +12,40 @@ const createLink = function () {
   return link;
 };
 
+const createButton = function () {
+  var button = document.createElement('a');
+  button.target = '_blank';
+  button.className = 'btn btn-dark btn-dark btn-sm';
+  button.style.marginInline = '3px';
+  return button;
+};
+
+const param = function (key) {
+  return encodeURI(findagrave[key]);
+};
+
 if (typeof findagrave !== 'undefined') {
+  var buttonContainer = document.querySelector('.form-group.hidden-print');
+  if (buttonContainer) {
+    var treeSearchButton = createButton();
+    treeSearchButton.innerText = 'Tree Search';
+    treeSearchButton.title = 'Check FamilySearch trees';
+    treeSearchButton.href = 'https://www.familysearch.org/tree/find/name?' +
+      `search=1&birth=%7C${param('birthYear')}-${param('birthYear')}%7C0` +
+      `&death=%7C${param('deathYear')}-${param('deathYear')}%7C0` +
+      `&self=${param('firstName')}%7C${param('lastName')}%7C0%7C0`;
+    buttonContainer.appendChild(treeSearchButton);
+
+    var recordSearchButton = createButton();
+    recordSearchButton.innerText = 'Record Search';
+    recordSearchButton.title = 'Check FamilySearch records';
+    recordSearchButton.href = 'https://www.familysearch.org/search/record/' +
+      `results?givenname=${param('firstName')}&surname=${param('lastName')}` +
+      `&birth_year_from=${param('birthYear')}&birth_year_to=${param('birthYear')}` +
+      `&death_year_from=${param('deathYear')}&death_year_to=${param('deathYear')}`;
+    buttonContainer.appendChild(recordSearchButton);
+  }
+
   var memorialElement = document.getElementById('memNumberLabel');
   if (memorialElement) {
     var familySearchLink = createLink();
@@ -23,17 +56,5 @@ if (typeof findagrave !== 'undefined') {
       `?external_record_id=${findagrave.memorialId}&collection_id=2221801`;
     memorialElement.innerHTML = '';
     memorialElement.appendChild(familySearchLink);
-  }
-
-  var nameElement = document.getElementById('bio-name');
-  if (nameElement) {
-    var familyTreeLink = createLink();
-    familyTreeLink.innerHTML = nameElement.innerHTML;
-    familyTreeLink.href = 'https://www.familysearch.org/tree/find/name?' +
-      `search=1&birth=%7C${findagrave.birthYear}-${findagrave.birthYear}%7C0` +
-      `&death=%7C${findagrave.deathYear}-${findagrave.deathYear}%7C0` +
-      `&self=${findagrave.firstName}%7C${findagrave.lastName}%7C0%7C0`;
-    nameElement.innerHTML = '';
-    nameElement.appendChild(familyTreeLink);
   }
 }
